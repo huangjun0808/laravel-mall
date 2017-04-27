42e7381bc675b7c8d0b276beb744acda2db62ed1
黄俊 <357569565@qq.com>
2017-04-27 12:49:34 +0800

    admin_user数据管理员填充

--- database/migrations/2017_04_26_052450_create_admin_users_table.php
+++ database/migrations/2017_04_26_052450_create_admin_users_table.php
@@ -18,6 +18,7 @@ class CreateAdminUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
---
+++ database/seeds/AdminUserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admin_user')->insert([
+            'name'=>'超级管理员',
+            'email'=>'root@admin.com',
+            'password'=>bcrypt('root123456'),
+            'created_at'=>date('Y-m-d H:i:s',time()),
+        ]);
+    }
+}
